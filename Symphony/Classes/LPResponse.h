c22cbe9168e959596fd1f5316375a7074c250503
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2019-04-02 14:06:56 -0700

    add regions to response

--- Symphony/Classes/LPResponse.h
+++ Symphony/Classes/LPResponse.h
@@ -8,20 +8,24 @@
 
 #import <Foundation/Foundation.h>
 
+@class LPRegion;
+
 @interface LPResponse : NSObject
-//@property (nonatomic, strong) LPActionDefinitions *actionDefinitions;
-//@property (nonatomic, strong) LPFileAttributes *fileAttributes;
-@property (nonatomic, copy)   NSArray *interfaceEvents;
-@property (nonatomic, copy)   NSArray *interfaceRules;
-@property (nonatomic, assign) BOOL isRegistered;
-@property (nonatomic, assign) BOOL isRegisteredFromOtherApp;
-//@property (nonatomic, strong) LPMessages *messages;
-//@property (nonatomic, strong) LPRegions *regions;
-@property (nonatomic, assign) BOOL isSuccess;
-@property (nonatomic, assign) BOOL isSyncNewsfeed;
-@property (nonatomic, copy)   NSString *token;
-@property (nonatomic, copy)   NSString *uploadURL;
-//@property (nonatomic, copy)   NSArray<LPVariant *> *variants;
-//@property (nonatomic, strong) LPResponseVars *vars;
-@property (nonatomic, copy)   NSDictionary<NSString *, id> *varsFromCode;
+
+@property (nonatomic, strong) NSArray<LPRegion *> *regions;
+
+////@property (nonatomic, strong) LPActionDefinitions *actionDefinitions;
+////@property (nonatomic, strong) LPFileAttributes *fileAttributes;
+//@property (nonatomic, copy)   NSArray *interfaceEvents;
+//@property (nonatomic, copy)   NSArray *interfaceRules;
+//@property (nonatomic, assign) BOOL isRegistered;
+//@property (nonatomic, assign) BOOL isRegisteredFromOtherApp;
+////@property (nonatomic, strong) LPMessages *messages;
+//@property (nonatomic, assign) BOOL isSuccess;
+//@property (nonatomic, assign) BOOL isSyncNewsfeed;
+//@property (nonatomic, copy)   NSString *token;
+//@property (nonatomic, copy)   NSString *uploadURL;
+////@property (nonatomic, copy)   NSArray<LPVariant *> *variants;
+////@property (nonatomic, strong) LPResponseVars *vars;
+//@property (nonatomic, copy)   NSDictionary<NSString *, id> *varsFromCode;
 @end
