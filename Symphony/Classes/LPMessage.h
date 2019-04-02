54eb5cf194c799208ebd494b61216b08ef427b1f
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2019-04-02 15:53:47 -0700

    add id

--- Symphony/Classes/LPMessage.h
+++ Symphony/Classes/LPMessage.h
@@ -12,12 +12,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface LPMessage : NSObject
 
+@property (nonatomic, strong) NSString *id;
 @property (nonatomic, strong) NSString *action;
 @property (nonatomic, assign) NSTimeInterval countdown;
 @property (nonatomic, assign) BOOL hasImpressionCriteria;
 @property (nonatomic, strong) NSString *parentCampaignId;
 @property (nonatomic, assign) int priority;
 @property (nonatomic, strong) NSArray *vars;
+@property (nonatomic, strong) NSArray *whenLimits;
 @property (nonatomic, strong) NSArray *whenTriggers;
 
 @end
