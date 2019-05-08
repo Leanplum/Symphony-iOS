896c7115530ed159459fe862f070c73985b48264
hrishi <hrishikesh.amravatkar@leanplum.com>
2019-05-08 12:40:03 -0700

    latest code changes for refactor

--- Leanplum/Api/LPUserApi.h
+++ Leanplum/Api/LPUserApi.h
@@ -14,10 +14,5 @@
                     success:(void (^)(void))success
                     failure:(void (^)(NSError *error))failure;
 
-//ToDo: Actual get needs to be discussed, this is for testing coverage.
-+ (void) getUsersAttributes:(NSString *)userId withUserAttributes:(NSDictionary *)attributes
-                    success:(void (^)(void))success
-                    failure:(void (^)(NSError *error))failure;
-
 @end
 
