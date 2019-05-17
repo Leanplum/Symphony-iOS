3ed887b5f2a459fde11da0f2ef49978576cabe5d
Grace Gu <grace.gu@leanplum.com>
2019-05-17 15:07:37 -0700

    add files

---
+++ Leanplum/Api/LPPauseSessionApi.h
@@ -0,0 +1,17 @@
+//
+//  LPPauseSessionApi.h
+//  Leanplum
+//
+//  Created by Grace on 5/17/19.
+//  Copyright © 2019 Leanplum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LPPauseSessionApi : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Leanplum/Api/LPPauseSessionApi.m
@@ -0,0 +1,13 @@
+//
+//  LPPauseSessionApi.m
+//  Leanplum
+//
+//  Created by Grace on 5/17/19.
+//  Copyright © 2019 Leanplum. All rights reserved.
+//
+
+#import "LPPauseSessionApi.h"
+
+@implementation LPPauseSessionApi
+
+@end
