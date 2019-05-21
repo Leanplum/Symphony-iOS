c0f4262c203a88be80d222914afbf4055a80da7c
hrishi <hrishikesh.amravatkar@leanplum.com>
2019-05-21 15:35:03 -0700

    network layer basic interface and code

--- LeanplumTests/LeanplumTests.m
+++ LeanplumTests/LeanplumTests.m
@@ -10,6 +10,7 @@
 #import <OCMock/OCMock.h>
 #import <OHHTTPStubs/OHHTTPStubs.h>
 #import "LPAPIConfig.h"
+#import "LPApiConstants.h"
 #import "LPTestHelper.h"
 #import "Leanplum.h"
 
@@ -30,25 +31,66 @@
     [OHHTTPStubs removeAllStubs];
 }
 
+- (void)testConfiguration
+{
+    NSString* host = @"test_host";
+    NSString* servlet = @"servlet";
+    
+    XCTAssertEqualObjects([LPApiConstants sharedState].apiHostName, @"api.leanplum.com");
+    XCTAssertEqualObjects([LPApiConstants sharedState].apiServlet, @"api");
+    
+    [Leanplum setApiHostName:host withServletName:servlet usingSsl:true];
+    
+    XCTAssertEqual([LPApiConstants sharedState].apiHostName, host);
+    XCTAssertEqual([LPApiConstants sharedState].apiServlet, servlet);
+    
+    [Leanplum setApiHostName:nil withServletName:nil usingSsl:true];
+    
+    XCTAssertEqual([LPApiConstants sharedState].apiHostName, host);
+    XCTAssertEqual([LPApiConstants sharedState].apiServlet, servlet);
+    
+    [Leanplum setApiHostName:host withServletName:nil usingSsl:true];
+    
+    XCTAssertEqual([LPApiConstants sharedState].apiHostName, host);
+    XCTAssertEqual([LPApiConstants sharedState].apiServlet, servlet);
+    
+    int timeout = 10;
+    
+    [Leanplum setNetworkTimeoutSeconds:timeout];
+    XCTAssertEqual([LPApiConstants sharedState].networkTimeoutSeconds, timeout);
+    XCTAssertEqual([LPApiConstants sharedState].networkTimeoutSecondsForDownloads, timeout);
+    
+    [Leanplum setNetworkTimeoutSeconds: -1];
+    XCTAssertEqual([LPApiConstants sharedState].networkTimeoutSeconds, timeout);
+    XCTAssertEqual([LPApiConstants sharedState].networkTimeoutSecondsForDownloads, timeout);
+    
+    [Leanplum setNetworkTimeoutSeconds:timeout forDownloads:timeout];
+    XCTAssertEqual([LPApiConstants sharedState].networkTimeoutSeconds, timeout);
+    XCTAssertEqual([LPApiConstants sharedState].networkTimeoutSecondsForDownloads, timeout);
+    
+    [Leanplum setNetworkTimeoutSeconds:20 forDownloads:-1];
+    XCTAssertEqual([LPApiConstants sharedState].networkTimeoutSeconds, timeout);
+    XCTAssertEqual([LPApiConstants sharedState].networkTimeoutSecondsForDownloads, timeout);
+    
+    [Leanplum setNetworkTimeoutSeconds:-1 forDownloads:20];
+    XCTAssertEqual([LPApiConstants sharedState].networkTimeoutSeconds, timeout);
+    XCTAssertEqual([LPApiConstants sharedState].networkTimeoutSecondsForDownloads, timeout);
+}
+
 /**
  * Tests whether setting user attributes and id works correctly.
  */
 - (void) testUserId
 {
+    [LPTestHelper setup];
     XCTestExpectation *expectation = [self expectationWithDescription:@"Query timed out."];
-    NSString *userId = @"john.smith";
-    NSDictionary *userAttributes = @{@"name": @"John Smith",
-                                     @"age": @42,
-                                     @"address": @"New York"
-                                     };
-    
     // Try to set user id and attributes.
     [Leanplum setUserId:DEVICE_ID withUserAttributes:nil withSuccess:^{
         [expectation fulfill];
     } withFailure:^(NSError *error) {
     }];
     
-    [self waitForExpectationsWithTimeout:40.0 handler:^(NSError *error) {
+    [self waitForExpectationsWithTimeout:20.0 handler:^(NSError *error) {
         if (error) {
             NSLog(@"Error: %@", error);
         }
@@ -60,9 +102,9 @@
  */
 - (void) testUserAttributesError
 {
+    [LPTestHelper setup];
     [LPTestHelper setupStub:400 withFileName:@"simple_error_success_response.json"];
     XCTestExpectation *expectation = [self expectationWithDescription:@"Query timed out."];
-    
     NSDictionary *userAttributes = @{@"name": @"John Smith",
                                      @"age": @42,
                                      @"address": @"New York"
@@ -86,6 +128,7 @@
  */
 - (void) testUserAttributesApiCall
 {
+    [LPTestHelper setup];
     XCTestExpectation *expectation = [self expectationWithDescription:@"Query timed out."];
     NSString *userId = @"john.smith";
     NSDictionary *userAttributes = @{@"name": @"John Smith",
