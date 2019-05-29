21d2358722e9555e2027c3b353ae1409573f0390
Mayank Sanganeria <mayank.ot@gmail.com>
2019-05-29 07:58:51 +0530

    E2-1938 stop (#34)

---
+++ Leanplum/Api/LPStopApi.h
@@ -0,0 +1,16 @@
+//
+//  LPStopApi.h
+//  Leanplum
+//
+//  Created by Mayank Sanganeria on 5/21/19.
+//  Copyright © 2019 Leanplum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LPStopApi : NSObject
+
++ (void) stopWithParameters:(NSDictionary *)parameters
+                success:(void (^)(void))success
+                failure:(void (^)(NSError *error))failure;
+@end
