fd90bf174a3575800322e07894e0be4f2697d50d
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2019-05-29 08:05:59 +0530

    E2-1947: add heartbeat (#28)

---
+++ Leanplum/Api/LPHeartbeatApi.m
@@ -0,0 +1,54 @@
+//
+//  LPHeartbeatApi.m
+//  Leanplum
+//
+//  Created by Mayank Sanganeria on 5/21/19.
+//  Copyright © 2019 Leanplum. All rights reserved.
+//
+
+#import "LPHeartbeatApi.h"
+#import "LPWSManager.h"
+#import "LPConstants.h"
+#import "LPApiConstants.h"
+#import "LPAPIConfig.h"
+#import "LPErrorHelper.h"
+#import "LPJSON.h"
+
+@implementation LPHeartbeatApi
+
++ (void) heartbeatWithParameters:(NSDictionary *)parameters
+                         success:(void (^)(void))success
+                         failure:(void (^)(NSError *error))failure {
+    void (^successResponse) (NSDictionary *) = ^(NSDictionary *response) {
+        NSError *error = nil;
+        NSArray *responseArray = [response valueForKey:@"response"];
+        NSDictionary *resultDict = responseArray[0];
+        if (error != nil) {
+            failure(error);
+        }
+        else {
+            if ([resultDict objectForKey:@"success"]) {
+                success();
+            } else {
+                NSError *error = [LPErrorHelper makeResponseError:resultDict];
+                failure(error);
+            }
+        }
+    };
+    void (^failureResponse) (NSError *) = ^(NSError *error ){
+        failure(error);
+    };
+
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+    if (parameters != nil) {
+        params = [parameters mutableCopy];
+    }
+    params[LP_PARAM_DEVICE_ID] = [LPAPIConfig sharedConfig].deviceId;
+    LPWSManager *wsManager = [[LPWSManager alloc] init];
+    [wsManager sendPOSTWebService:LP_API_METHOD_HEARTBEAT
+                       withParams:params
+                     successBlock:successResponse
+                     failureBlock:failureResponse];
+}
+
+@end
